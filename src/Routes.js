0e2f09a7a6acd0d9e10bfd41674b3af6bfe00034
shashinbhayani <shashinbhayani@gmail.com>
2019-09-23 16:33:40 +0530

    removed unused libraries

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import { createAppContainer, createSwitchNavigator, createStackNavigator } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createAppContainer, createSwitchNavigator, createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
-import Ionicons from 'react-native-vector-icons/Ionicons';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
 import Videos from './screens/Videos';
 import Strips from './screens/Strips';
 import Profile from './screens/Profile';
 
-const HomeRoutes = createBottomTabNavigator({
+const HomeRoute = createBottomTabNavigator({
   Videos: Videos,
   Strips: Strips,
   Profile: Profile
 })
 
-const AuthRoute = createAppContainer(createStackNavigator({
+const AuthRoute = createStackNavigator({
   Login: Login,
   Signup: Signup,
 }, {
@@ -23,12 +20,12 @@ const AuthRoute = createAppContainer(createStackNavigator({
     navigationOptions: {
       headerVisible: false,
     },
-}))
+})
 
 const Routes = createSwitchNavigator(
   {
     Auth: AuthRoute,
-    Home: HomeRoutes
+    Home: HomeRoute
   },
   {
     initialRouteName: 'Auth',
